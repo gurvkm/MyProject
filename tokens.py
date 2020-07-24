83a637092cd8f29ac3f692fde7881ccc7a8d0570
GAURAV KUMAR MISHRA <gurvkm@gmail.com>
2020-07-24 11:48:32 +0530

    token
    How to extract the tokens of a sentence.

--- tokens.py
+++ tokens.py
@@ -1,9 +1,11 @@
+# Devide the words into tokens
+
 from nltk.tokenize import sent_tokenize,word_tokenize
 
 
 
 example_text="Hello Mr. Smith, how are you doing today? The weather is great and python is awsome. The sky is pink."
-#print(sent_tokenize(example_text))
-#print(word_tokenize(example_text))
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
 for i in word_tokenize(example_text):
 	print(i)
